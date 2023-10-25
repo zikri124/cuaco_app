5c7a508c944d5d469ca8ab2f0eeebf734c624496
zikri124 <faisalsaja73@gmail.com>
2023-10-25 23:55:38 +0700

    fix: cannot show color in uvi indicator, clear the image unused, uninstall dependencies unused

--- app/components/SunCard.tsx
+++ app/components/SunCard.tsx
@@ -17,28 +17,22 @@ export default function SunCard({ uvi, sunriseT, sunsetT }: props) {
 
     var message = ""
     var category = ""
-    var color = ""
 
     if (uvi < 3) {
         category = "Low"
         message = messageArray[0]
-        color = "green-300"
     } else if (uvi < 6) {
         category = "Medium"
         message = messageArray[1]
-        color = "yellow-300"
     } else if (uvi < 8) {
         category = "High"
         message = messageArray[2]
-        color = "orange-300"
     } else if (uvi < 11) {
         category = "Very High"
         message = messageArray[3]
-        color = "orange-600"
     } else {
         category = "Extreme"
         message = messageArray[4]
-        color = "purple-300"
     }
 
     return (
@@ -52,25 +46,27 @@ export default function SunCard({ uvi, sunriseT, sunsetT }: props) {
                 </div>
             </div>
 
-            <div className='flex flex-col items-center mt-4 bg-white px-4 py-4 rounded-2xl'>
-                <div className="grid grid-cols-2 items-center w-full text-center">
-                    <h4 className="col-span-1 text-2xl font-semibold">UV Index</h4>
+            <div className='flex flex-col items-center mt-4 bg-white p-2 rounded-2xl'>
+                <div className="grid grid-cols-2 items-center w-full text-center py-2">
+                    <h4 className="col-span-1 text-lg px-2 font-semibold">Ultraviolet Index</h4>
                     <div className='col-span-1 flex items-center gap-2 justify-center'>
                         <h2 className='text-5xl font-semibold'>{uvi} </h2>
-                        <div className={'text-sm font-semibold px-4 py-1 rounded-full bg-' + color}>{category}</div>
+                        <div className={`text-sm font-semibold px-4 py-1 rounded-full border-2 border-amber-300`}>{category}</div>
                     </div>
                 </div>
-                <p className='bg-white rounded-full font-semibold mt-4 text-sm'>{message}</p>
+                <div className="bg-white rounded-xl border-2 border-amber-300 w-full h-full text-center mt-2 py-2 px-2">
+                    <p className='font-semibold text-sm'>{message}</p>
+                </div>
             </div>
 
             <div className='mt-2 text-center flex justify-between items-center text-sm px-2'>
-                <div className='rounded-2xl py-2 pe-6 flex flex-col items-center'>
+                <div className='rounded-2xl pe-6 flex flex-col items-center'>
                     <Image src={"https://img.icons8.com/stickers/100/sunrise.png"} height={50} width={50} alt='sunrise-logo' />
                     <p className='font-semibold'>Sunrise</p>
                     <p className="w-max">{sunriseT}</p>
                 </div>
                 <div className='flex-grow w-full border-2 border-dashed border-yellow-800'></div>
-                <div className='rounded-2xl py-2 ps-6 flex flex-col items-center'>
+                <div className='rounded-2xl ps-6 flex flex-col items-center'>
                     <Image src={"https://img.icons8.com/stickers/100/sunset.png"} height={50} width={50} alt='sunset-logo' />
                     <p className='font-semibold'>Sunset</p>
                     <p className="w-max">{sunsetT}</p>
